a039d4cd1a5435ed723416f5faee9962a4ecae2a
marcodelgado <marcodelgadoaccess@gmail.com>
2022-03-17 19:26:51 -0400

    Update to Nvidia Policy Env (50 hertz)
    robot walking step function runs at 50 hertz.
    updated the standing position to reflect those the Nvidia policy ran on.

--- locomotion/envs/walking_locomotion.py
+++ locomotion/envs/walking_locomotion.py
@@ -1,26 +1,29 @@
 from locomotion.envs.locomotion_gym_env import LocomotionGymEnv
 
+import time
 import numpy as np
-import pybullet_data as pd
 
 SWITCHED_POSITIONS = [3, 4, 5, 0, 1, 2, 9, 10, 11, 6, 7, 8]
-# STANDING_POSITION = np.array([0, 0.432, -0.77] * 4)
-STANDING_POSITION = np.array([0, np.deg2rad(40), -np.deg2rad(80)] * 4)
 JOINT_LIMITS_ENERGY = np.array([0.15, 0.4, 0.4] * 4)
 
 
 class LocomotionWalk(LocomotionGymEnv):
     def __init__(self, gym_config, robot_class=None, is_render=False, on_rack=False):
         super().__init__(gym_config, robot_class, is_render, on_rack)
+        self.STANDING_POSITION = LocomotionGymEnv.deafult_dof_pos
 
     def step(self, action):
+        start_time = time.time()
         action = np.array(action)
 
-        deltas = action # * JOINT_LIMITS_ENERGY + STANDING_POSITION
+        deltas = action * JOINT_LIMITS_ENERGY + self.STANDING_POSITION
 
         self._robot.Step(deltas)
 
         observations = self._get_observation()
         # observations = np.concatenate(list(observations.values()))
+
+        while time.time() - start_time <= 1 / 50:
+            pass
 
         return observations, 0, False, {}

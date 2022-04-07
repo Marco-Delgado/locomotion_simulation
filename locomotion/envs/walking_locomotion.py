b0e4ab79604733b8e234d833221066f747af3f6f
marcodelgado <marcodelgadoaccess@gmail.com>
2022-04-07 15:49:29 -0400

    Walking locomotion and Robot changes
    Moved standing function from robot reset to walking_locomotion.py's init function. This will put the robot in default_dof position (standing position) before starting the walking policy. This will allow it to be positioned correctly before running the policy.
    
    Changed the step function motor_control_mode to always be set to position.
    
    Created an observation for when the robot is on the rack. Artificially set velocities to make the robot believe it is on the ground moving.

--- locomotion/envs/walking_locomotion.py
+++ locomotion/envs/walking_locomotion.py
@@ -1,4 +1,6 @@
 from locomotion.envs.locomotion_gym_env import LocomotionGymEnv
+from locomotion.robots import robot_config
+from tqdm import tqdm
 
 import torch
 import time
@@ -10,6 +12,7 @@ clip = 100
 
 class LocomotionWalk(LocomotionGymEnv):
     def __init__(self, gym_config, robot_class, is_render=False, on_rack=False):
+        self.action = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         self.default_motor_angle = np.array(
             [
                 0.1000,
@@ -26,18 +29,30 @@ class LocomotionWalk(LocomotionGymEnv):
                 -1.5000,
             ]
         )
-        self.action = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         super().__init__(gym_config, robot_class, is_render, on_rack)
+
+        current_motor_angle = np.array(self.get_motor_angles())
+        desired_motor_angle = self.default_motor_angle
+
+        # Sets the robot to stand using blending
+        for t in tqdm(range(500)):
+            blend_ratio = np.minimum(t / 200.0, 1)
+            action = (
+                1 - blend_ratio
+            ) * current_motor_angle + blend_ratio * desired_motor_angle
+
+            self._robot.Step(action)
 
     def step(self, action):
         start_time = time.time()
         self.action = action
 
         action = np.array(action[SWITCHED_POSITIONS])
-        action = np.clip(action, -clip, clip)
 
         joint_angles = action + self.default_motor_angle
-        self._robot.Step(joint_angles)
+        self._robot.Step(
+            joint_angles, motor_control_mode=robot_config.MotorControlMode.POSITION
+        )
 
         while time.time() - start_time <= 1 / 60:
             pass
@@ -63,6 +78,30 @@ class LocomotionWalk(LocomotionGymEnv):
                 ),
                 torch.tensor(
                     self.get_motor_velocities(),
+                    dtype=torch.float,
+                ),
+                torch.tensor(self.action, dtype=torch.float),
+            ),
+            dim=-1,
+        )
+        return observations
+
+    def get_observation_rack(self):
+        observations = torch.cat(
+            (
+                torch.tensor([0.4, 0, 0], dtype=torch.float),
+                torch.tensor(
+                    [0, 0, 0],
+                    dtype=torch.float,
+                ),
+                torch.tensor(self._robot.GetProjectedGravity(), dtype=torch.float),
+                torch.tensor(self._robot.GetDirection(), dtype=torch.float),
+                torch.tensor(
+                    self.get_motor_angles() - self.default_motor_angle,
+                    dtype=torch.float,
+                ),
+                torch.tensor(
+                    self._robot.GetMotorVelocities(),
                     dtype=torch.float,
                 ),
                 torch.tensor(self.action, dtype=torch.float),
--- locomotion/robots/aliengo_robot.py
+++ locomotion/robots/aliengo_robot.py
@@ -66,13 +66,6 @@ _DEFAULT_HIP_POSITIONS = (
     (-0.195, 0.13, 0),
 )
 
-ABDUCTION_P_GAIN = 230.0
-ABDUCTION_D_GAIN = 20.0
-HIP_P_GAIN = 230.0
-HIP_D_GAIN = 20.0
-KNEE_P_GAIN = 230.0
-KNEE_D_GAIN = 20.0
-
 COMMAND_CHANNEL_NAME = "LCM_Low_Cmd"
 STATE_CHANNEL_NAME = "LCM_Low_State"
 
@@ -154,8 +147,25 @@ class AliengoRobot(aliengo.Aliengo):
     def __init__(self, pybullet_client, time_step=0.002, **kwargs):
         """Initializes the robot class."""
         # Initialize pd gain vector
-        self.motor_kps = np.array([ABDUCTION_P_GAIN, HIP_P_GAIN, KNEE_P_GAIN] * 4)
-        self.motor_kds = np.array([ABDUCTION_D_GAIN, HIP_D_GAIN, KNEE_D_GAIN] * 4)
+
+        # AIR GAINS
+        self.ABDUCTION_P_GAIN = 6.0
+        self.ABDUCTION_D_GAIN = 4.0
+        self.HIP_P_GAIN = 6.0
+        self.HIP_D_GAIN = 15.0
+        self.KNEE_P_GAIN = 6.0
+        self.KNEE_D_GAIN = 15.0
+
+        # LAND GAINS
+        # self.ABDUCTION_P_GAIN = 25.0
+        # self.ABDUCTION_D_GAIN = 0.0
+        # self.HIP_P_GAIN = 65.0
+        # self.HIP_D_GAIN = 5.0
+        # self.KNEE_P_GAIN = 90.0
+        # self.KNEE_D_GAIN = 10.0
+
+        self.motor_kps = np.array([self.ABDUCTION_P_GAIN, self.HIP_P_GAIN, self.KNEE_P_GAIN] * 4)
+        self.motor_kds = np.array([self.ABDUCTION_D_GAIN, self.HIP_D_GAIN, self.KNEE_D_GAIN] * 4)
         self._pybullet_client = pybullet_client
         self.time_step = time_step
 
@@ -266,7 +276,7 @@ class AliengoRobot(aliengo.Aliengo):
         return self._base_orientation.copy()
 
     def GetDirection(self):
-        return np.array([1.0, 0.0, 0.0])
+        return np.array([0.4, 0.0, 0.0])
 
     @property
     def motor_velocities(self):
@@ -296,22 +306,6 @@ class AliengoRobot(aliengo.Aliengo):
             reset_time=-1,
         )
         logging.warning("About to reset the robot, make sure the robot is hang-up.")
-
-        if default_motor_angles is None:
-            default_motor_angles = aliengo.INIT_MOTOR_ANGLES
-
-        current_motor_angles = self.GetMotorAngles()
-
-        # Stand up in 1.5 seconds, and keep the behavior in this way.
-        standup_time = min(reset_time, 1.5)
-        for t in np.arange(0, reset_time, self.time_step * self._action_repeat):
-            blend_ratio = min(t / standup_time, 1)
-            action = (
-                blend_ratio * default_motor_angles
-                + (1 - blend_ratio) * current_motor_angles
-            )
-            self.Step(action, robot_config.MotorControlMode.POSITION)
-            time.sleep(self.time_step * self._action_repeat)
 
         if self._enable_action_filter:
             self._ResetActionFilter()
